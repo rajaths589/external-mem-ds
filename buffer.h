493add0924e239ac30142d165aca4c85d91b1774
Rajath Shashidhara <rajath.shashidhara@gmail.com>
2015-12-23 12:57:54 +0530

    Class Declaration added

---
+++ buffer.h
@@ -0,0 +1,18 @@
+
+template <typename BlkSize>
+class BufferedFile {
+private:
+	int fd;
+	long lastblk;	
+	inline long getblockoffset(long blknbr);
+public:
+	BufferedFile(const char* filepath);
+	~BufferedFile();
+	void readBlock(long blknbr, void* buffer);
+	void writeBlock(long blknbr, void* buffer);
+	long allotBlock();
+	void readHeader(void* buffer);
+	void writeHeader(void* buffer);
+	void deleteBlock(long blknbr);
+	void deletefile();
+};
